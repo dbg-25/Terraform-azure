fa87bfb9dd725d4d179b922596d8053f4ebee8a1
Donovan <Donovangriffin04@gmail.com>
2026-01-29 07:12:07 -0800

    Security polucy etc

--- firewall/main.tf
+++ firewall/main.tf
@@ -12,42 +12,91 @@ provider "azurerm" {
 }
 
 
-variable "rules" {
+
+variable "team_access" {
   type = list(object({
-    port      = string
-    name      = string
-    direction = string
-    priority  = number # Added priority as it is required for NSGs
+    team_name          = string
+    object_id          = string
+    secret_permissions = list(string)
   }))
 
   default = [
     {
-      port      = "80"
-      name      = "Allow-HTTP"
-      direction = "Inbound"
-      priority  = 100
+      team_name          = "Admins"
+      object_id          = "11111111-1111-1111-1111-111111111111"
+      secret_permissions = ["Get", "List", "Set", "Delete"]
     },
     {
-      port      = "22"
-      name      = "Allow-SSH"
-      direction = "Inbound" # Fixed direction (Outbound for SSH usually doesn't make sense for ingress rules here)
-      priority  = 110
+      team_name          = "Developers"
+      object_id          = "22222222-2222-2222-2222-222222222222"
+      secret_permissions = ["Get", "List"]
     }
   ]
 }
 
+data "azurerm_client_config" "current" {}
 
-resource "azurerm_network_security_group" "MyNSG" {
-  name = "NSG4L"
+resource "azurerm_resource_group" "rg" {
+  name     = "vault-rg"
   location = "Canada Central"
-  resource_group_name = "RG1"
-
-  dynamic "security_rule" {
-    for_each = var.rules
-    content {
-      source_port_range = security_rule.value.port
-      name = security_rule.value.name
-      direction = security_rule.value.direction
-    }
 }
+
+resource "azurerm_key_vault" "main" {
+  name                = "my-app-vault-xyz"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  tenant_id           = data.azurerm_client_config.current.tenant_id
+  sku_name            = "standard"
+
+dynamic "access_policy" {
+  for_each = var.team_access
+  iterator = team
+
+  content {
+    tenant_id = data.azurerm_client_config.current.tenant_id
+    object_id = team.value.object_id
+    secret_permissions = team.value.secret_permissions
+
+  }
+}
+
+}
+
+variable "subnet_config" {
+  type = map(object({
+    cidr_range = string
+    nsg_id     = string
+  }))
+
+  default = {
+    "Frontend" = {
+      cidr_range = "10.0.1.0/24"
+      nsg_id     = "/subscriptions/.../nsg-front"
+    },
+    "Backend" = {
+      cidr_range = "10.0.2.0/24"
+      nsg_id     = "/subscriptions/.../nsg-back"
+    }
+  }
+}
+
+resource "azurerm_virtual_network" "main" {
+  name                = "production-vnet"
+  location            = "Canada Central"
+  resource_group_name = "Networking-RG"
+  address_space       = ["10.0.0.0/16"]
+
+  dynamic "subnet"{
+    for_each = var.subnet_config
+    iterator = sub
+
+    content {
+      name = sub.Key
+      address_prefixes = sub.value.cidr_range
+      security_group = sub.value.nsg_id
+    }
+  }
+
+
+
 }
